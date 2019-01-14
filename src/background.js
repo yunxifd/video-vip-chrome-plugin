d4e1fc8615eb42f1aed62845fba6d262fd40df09
yunxifd <yunxifd@outlook.com>
2019-01-14 17:43:07 +0800

    初步实现右键菜单功能

--- src/background.js
+++ src/background.js
@@ -1,22 +1,51 @@
-function getBadgeTextCallback(badgeText) {
-    if (!badgeText || badgeText === 'off') {
-        chrome.browserAction.setBadgeText({ text: "on" });
-        chrome.browserAction.setBadgeBackgroundColor({
-            color: "green"
-        })
-    } else {
-        chrome.browserAction.setBadgeText({ text: "off" });
-        chrome.browserAction.setBadgeBackgroundColor({
-            color: "red"
-        })
-    }
+var contexts = ["page", "link"];
+
+function onClickMenu(info, tab) {
+    console.log(info, tab);
 }
 
-function toggleState() {
-    console.log("test");
-    chrome.browserAction.getBadgeText({}, getBadgeTextCallback)
-};
+function getProfiles() {
+    var profiles = localStorage.getItem("profiles") || "";
+    var list = profiles.split('\n');
+    var result = [];
+    list.forEach(item => {
+        if (item) {
+            var tmp = item.split("$");
+            if (tmp.length === 2) {
+                result.push({
+                    name: tmp[0],
+                    url: tmp[1]
+                })
+            } else {
+                console.error("配置数据，格式错误，目前只支持 \{\name}\$\{url\}格式")
+            }
+        }
+    })
+    return result;
+}
 
-chrome.browserAction.onClicked.addListener(toggleState);
+const profiles = getProfiles();
 
-toggleState();
+function openNewTab(interfaceUrl, linkUrl) {
+    var url = interfaceUrl + linkUrl;
+    chrome.tabs.create({
+        url: url
+    })
+}
+
+profiles.forEach((p, index) => {
+    var title = p.name || "接口" + (index + 1)
+    chrome.contextMenus.create({
+        id: title,
+        title: title,
+        contexts: contexts
+    })
+})
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    var linkUrl = info.linkUrl || info.pageUrl;
+    var index = 0;
+    if (info.menuItemId == 'defaultMenu') {
+        index = 0;
+    }
+    console.log(info, tab);
+})
